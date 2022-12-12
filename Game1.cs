6c0892115be81de2dd83b77ecaf69643826b61f2
Doom-Art <ouginak121@gmail.com>
2022-12-12 08:34:54 -0500

    done

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Pacman_monogame_8
 {
@@ -23,7 +24,7 @@ namespace Pacman_monogame_8
         Texture2D barrierTexture;
         Rectangle barrierRect1, barrierRect2;
         Texture2D coinTexture;
-        Rectangle coinRect;
+        List<Rectangle> coins;
         int pacSpeed;
 
         public Game1()
@@ -41,7 +42,11 @@ namespace Pacman_monogame_8
             pacRect = new Rectangle(10, 10, 60, 60);
             barrierRect1 = new Rectangle(0, 250, 350, 75);
             barrierRect2 = new Rectangle(450, 250, 350, 75);
-            coinRect = new Rectangle(400, 50, coinTexture.Width, coinTexture.Height);
+            coins = new List<Rectangle>();
+            coins.Add(new Rectangle(400, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(475, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(200, 400, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(400, 400, coinTexture.Width, coinTexture.Height));
             exitRect = new Rectangle(700, 380, 100, 100);
         }
 
@@ -66,31 +71,60 @@ namespace Pacman_monogame_8
         protected override void Update(GameTime gameTime)
         {
             keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             if (keyboardState.IsKeyDown(Keys.Left))
             {
                 pacRect.X -= pacSpeed;
                 currentPacTexture = pacLeftTexture;
+                if (pacRect.Intersects(barrierRect1))
+                    pacRect.X = barrierRect1.Right;
             }
             if (keyboardState.IsKeyDown(Keys.Right))
             {
                 pacRect.X += pacSpeed;
                 currentPacTexture = pacRightTexture;
+                if (pacRect.Intersects(barrierRect1)){
+                    pacRect.X = barrierRect1.Left - pacRect.Width;
+                }
+                if (pacRect.Intersects(barrierRect2)){
+                    pacRect.X = barrierRect2.Left - pacRect.Width;
+                }
             }
             if (keyboardState.IsKeyDown(Keys.Up))
             {
                 pacRect.Y -= pacSpeed;
                 currentPacTexture = pacUpTexture;
+                if (pacRect.Intersects(barrierRect1)){
+                    pacRect.Y = barrierRect1.Bottom;
+                }
+                if (pacRect.Intersects(barrierRect2)){
+                    pacRect.Y = barrierRect2.Bottom;
+                }
             }
             if (keyboardState.IsKeyDown(Keys.Down))
             {
                 pacRect.Y += pacSpeed;
                 currentPacTexture = pacDownTexture;
+                if (pacRect.Intersects(barrierRect1)){
+                    pacRect.Y = barrierRect1.Top - pacRect.Height;
+                }
+                if (pacRect.Intersects(barrierRect2)){
+                    pacRect.Y = barrierRect2.Top - pacRect.Height;
+                }
             }
-            if (pacRect.Intersects(coinRect))
+            if (mouseState.LeftButton == ButtonState.Pressed)
+                if (exitRect.Contains(mouseState.X, mouseState.Y))
+                    Exit();
+            if (exitRect.Contains(pacRect))
+                Exit();
+            for (int i = 0; i < coins.Count; i++)
             {
-                coinRect.Location = new Point(800, 480);
+                if (pacRect.Intersects(coins[i])){
+                    coins.RemoveAt(i);
+                    i--;
+                }
             }
             // TODO: Add your update logic here
 
@@ -106,7 +140,10 @@ namespace Pacman_monogame_8
             _spriteBatch.Draw(barrierTexture, barrierRect2, Color.White);
             _spriteBatch.Draw(exitTexture, exitRect, Color.White);
             _spriteBatch.Draw(currentPacTexture, pacRect, Color.White);
-            _spriteBatch.Draw(coinTexture, coinRect, Color.White);
+            foreach (Rectangle coinRect in coins)
+            {
+                _spriteBatch.Draw(coinTexture, coinRect, Color.White);
+            }
 
             _spriteBatch.End();
             // TODO: Add your drawing code here
